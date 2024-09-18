492ad27af36c7e143769c361d5c5f912ee54c020
dejito <Deerealboy@gmail.com>
2024-09-18 12:35:29 +0100

    Initial Commit

---
+++ LoginUI/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  LoginUI
+//
+//  Created by MAC on 9/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                
+
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LoginUI/LoginUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginUIApp.swift
+//  LoginUI
+//
+//  Created by MAC on 9/18/24.
+//
+
+import SwiftUI
+
+@main
+struct LoginUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
